7cc4f398e0653b0f8c52534fc8eaac5ffccf1d96
emsammz-dev <hiteshemsammzdev@gmail.com>
2024-05-23 21:23:50 +0530

    dashboard-nextjs

---
+++ my-next-app1/src/app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Create Next App",
+  description: "Generated ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
+
+
+
